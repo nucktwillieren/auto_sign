2c1b089afe1001733e86684ebb4188ed69ef98a5
nucktwillieren <32989922+nucktwillieren@users.noreply.github.com>
2019-07-31 01:40:02 +0800

    init

--- main.py
+++ main.py
@@ -2,83 +2,137 @@ import requests
 import pprint
 from bs4 import BeautifulSoup as bs
 import ast
+import json
+import random
+import schedule
+import time
+import threading
 
-class Login:
-    s = requests.session()
-    r = s.get('https://portal.ncu.edu.tw/login')
+def timer():
+    while 1:
+        try:
+            print(time.strftime("%c"))
+            time.sleep(3600)
+        except:
+            break
 
-    NID = s.cookies['NID']
-    j_session_id = s.cookies['JSESSIONID']
-
-    username = input('username: ')
-    password = input('password: ')
-    job_content = input('job_content: ')
-
-    s.post(
-        "https://portal.ncu.edu.tw/login/j_spring_security_check",
-        headers=dict(Referer="https://cis.ncu.edu.tw/HumanSys/home"),
-        data={
-            "j_username":username,
-            "j_password":password
-        }
-    )
-
-    res = s.get('https://cis.ncu.edu.tw/HumanSys/student/stdSignIn')
-    soup = bs(res.text,'html.parser')
-    a_tags = soup.find_all('a')
-    for tag in a_tags:
-        if tag.text == '新增簽到':
-            href = tag['href']
-
-    res = s.get("https://cis.ncu.edu.tw/HumanSys/student/{}&msg=signin_ok".format(href))
-    soup = bs(res.text,'html.parser')
-    input_tags = soup.find_all('input')
-    in_or_out = 'in'
-    for tag in input_tags:
-        tag_id = tag.get('id')
-        if tag_id == 'idNo':
-            id_no = tag['value']
-        if tag_id == 'ParttimeUsuallyId':
-            parttime_id = tag['value']
-        if tag_id == 'signout':
-            in_or_out = 'out'
-        if 'name' in tag.attrs:
-            if tag['name'] == '_token':
-                token = tag['value']
-
-    #print('idNo: ', no_id)
-    #print('partim_id: ', parttime_id)
-    #print('_token:',token)
-
-    data = {
-        "functionName": "doSign",
-        "idNo": id_no,
-        "ParttimeUsuallyId": parttime_id,
-        "AttendWork": job_content,
-        "_token":token
-    }
-
+class NcuWeb:
+    def __init__(self,username,password):
+        self._username = username
+        self._password = password
+        self.sign_href = ''
+        with open('job_content.json') as f:
+            self._job_content_json = json.load(f)
+        
+    def _restart_ncu_session(self):
+        self.s = requests.session()
+        self.r = self.s.get('https://portal.ncu.edu.tw/login')
+        self.NID = self.s.cookies['NID']
+        self.j_session_id = self.s.cookies['JSESSIONID']
     
-    r = s.post(
-        "https://cis.ncu.edu.tw/HumanSys/student/stdSignIn_detail",
-        headers=dict(Referer="https://cis.ncu.edu.tw/HumanSys/student/{}&msg=signin_ok".format(href)),
-        data=data
-    )
-    soup = bs(
-        r.text,
-        'html.parser'
-    )
+    def login(self):
+        self._restart_ncu_session()
+        self.s.post(
+            "https://portal.ncu.edu.tw/login/j_spring_security_check",
+            headers=dict(Referer="https://cis.ncu.edu.tw/HumanSys/home"),
+            data={
+                "j_username":self._username,
+                "j_password":self._password
+            }
+        )
+    
+    def _get_sign_href(self):
+        res = self.s.get('https://cis.ncu.edu.tw/HumanSys/student/stdSignIn')
+        soup = bs(res.text,'html.parser')
+        a_tags = soup.find_all('a')
+        for tag in a_tags:
+            if tag.text == '新增簽到':
+                self.sign_href = tag['href']
 
-    try:
-        if ast.literal_eval(soup.prettify())['isOK'] == "Y":
-            print('sign {}'.format(in_or_out),' OK')
-        else:
-            print('Error')
-    except:
-        print('Error')
+    def _get_sign_object(self):
+        self._get_sign_href()
+        res = self.s.get("https://cis.ncu.edu.tw/HumanSys/student/{}&msg=signin_ok".format(self.sign_href))
+        soup = bs(res.text,'html.parser')
+        input_tags = soup.find_all('input')
+
+        self.in_or_out = 'in'
+        for tag in input_tags:
+            tag_id = tag.get('id')
+            if tag_id == 'idNo':
+                self.id_no = tag['value']
+            if tag_id == 'ParttimeUsuallyId':
+                self.parttime_id = tag['value']
+            if tag_id == 'signout':
+                self.in_or_out = 'out'
+            if 'name' in tag.attrs:
+                if tag['name'] == '_token':
+                    self.sign_token = tag['value']
+
+        #print('idNo: ', self.id_no)
+        #print('partim_id: ', self.parttime_id)
+        #print('_token:',self.sign_token)
+
+    def _get_job_content(self):
+        self.job_content = self._job_content_json[random.randint(0,len(self._job_content_json)-1)]                        
+
+    def sign(self,in_or_out):
+        if in_or_out == 'reset':
+            self.login()
+            
+        self._get_sign_object()
+        self._get_job_content()          
+
+        data = {
+            "functionName": "doSign",
+            "idNo": self.id_no,
+            "ParttimeUsuallyId": self.parttime_id,
+            "AttendWork": self.job_content,
+            "_token":self.sign_token
+        }
+
+        
+        res = self.s.post(
+            "https://cis.ncu.edu.tw/HumanSys/student/stdSignIn_detail",
+            headers=dict(Referer="https://cis.ncu.edu.tw/HumanSys/student/{}&msg=signin_ok".format(self.sign_href)),
+            data=data
+        )
+        soup = bs(
+            res.text,
+            'html.parser'
+        )
+
+        try:
+            if ast.literal_eval(soup.prettify())['isOK'] == "Y":
+                print(time.strftime("%c"), ' sign {}'.format(self.in_or_out),' OK')
+            else:
+                print(time.strftime("%c"), ' Error')
+        except:
+            print(time.strftime("%c"), ',Error')
+        
+        if in_or_out != 'reset':
+            if self.in_or_out.upper() != in_or_out.upper():
+                self.sign('reset')
+                time.sleep(2)  
+
+def job(username,password,in_or_out):
+    n = NcuWeb(username,password)
+    n.login()
+    n.sign(in_or_out)
     
 def main():
-    l = Login()
+    username = input('username: ')
+    password = input('password: ')
+    threading.Thread(target=timer).start()
+    #job(username,password,'out')
+    #time.sleep(3)
+    #job(username,password,'out')
+    
+    schedule.every().day.at("08:30").do(job,username=username,password=password,in_or_out='in')
+    schedule.every().day.at("20:00").do(job,username=username,password=password,in_or_out='out')
+
+    while 1:
+        schedule.run_pending()
+        time.sleep(300)
 
 if __name__ == '__main__':
     main()
